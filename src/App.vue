81a828a1a89c4ab85c42d1fcabd052c5841a4afe
Владимир Печатнов <pechatnov.v@gmail.com>
2022-08-17 14:46:41 +0300

    Создание проекта

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
-</template>
+<template>1</template>
 
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
-export default {
-  name: "App",
-  components: {
-    HelloWorld: HelloWorldVue,
-  },
-};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
