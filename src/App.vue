99dfe10beec18956000f15993020bb1221ff5396
Владимир Печатнов <pechatnov.v@gmail.com>
2022-08-17 15:03:10 +0300

    Methods. V-ON. Слушатели событий

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
-<template>1</template>
+<template>
+  <div>
+    <div>
+      <button v-on:click="addLike">Лайк</button>
+      <button v-on:click="addDisLike">Дизлайк</button>
+    </div>
+    <div>
+      Количество лайков: <b>{{ likes }}</b>
+    </div>
+    <div>
+      Количество дизлайков: <b>{{ dislikes }}</b>
+    </div>
+  </div>
+</template>
 
 <script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDisLike() {
+      this.dislikes -= 1;
+    },
+  },
+};
 </script>
 
 <style>
