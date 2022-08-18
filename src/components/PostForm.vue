baf0e658840767db91bb2b0f2ba0ed217799d40c
Владимир Печатнов <pechatnov.v@gmail.com>
2022-08-18 17:16:03 +0300

    Подробно о V-MODEL

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,18 +1,8 @@
 <template>
   <form @submit.prevent>
     <h4>Создание поста</h4>
-    <input
-      v-model="post.title"
-      class="input"
-      type="text"
-      placeholder="Название"
-    />
-    <input
-      v-model="post.body"
-      class="input"
-      type="text"
-      placeholder="Описание"
-    />
+    <my-input v-model="post.title" type="text" placeholder="Название" />
+    <my-input v-model="post.body" type="text" placeholder="Описание" />
     <my-button
       style="align-self: flex-end; margin-top: 15px"
       @click="createPost"
@@ -48,11 +38,5 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-}
-.input {
-  width: 100%;
-  border: 2px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
 }
 </style>
