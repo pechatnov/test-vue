7c164f332364b2f49799298e820569a0a065ee89
Владимир Печатнов <pechatnov.v@gmail.com>
2022-08-18 10:02:13 +0300

    Двустороннее связывание

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    <form>
+    <form @submit.prevent>
       <h4>Создание поста</h4>
       <input
         v-bind:value="title"
-        @input="inputTitle"
+        @input="title = $event.target.value"
         class="input"
         type="text"
         placeholder="Название"
       />
       <input
         v-bind:value="body"
+        @input="body = $event.target.value"
         class="input"
         type="text"
         placeholder="Описание"
@@ -38,9 +39,15 @@ export default {
     };
   },
   methods: {
-    createPost() {},
-    inputTitle(event) {
-      console.log(event);
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
     },
   },
 };
