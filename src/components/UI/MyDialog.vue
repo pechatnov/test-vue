9298122f4fbc4f57e55a65b250a87ffa0042a22d
Владимир Печатнов <pechatnov.v@gmail.com>
2022-08-19 16:28:29 +0300

    Отрисовка по условию. Модальное окно.

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="show === true" class="dialog">
+    <div class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-dialog",
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
